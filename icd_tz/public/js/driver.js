d422743a5dcbde5e4a25893ad0ecc233c37d2e85
av-dev2 <emgani@aakvatech.com>
2026-07-07 21:02:28 +0300

    feat(frontend): implement supplier set_query filters
    - Added JS filters to restrict supplier selection to those with
    - Registered new custom scripts for standard Driver and Vehicle DocTypes in hooks.py

---
+++ icd_tz/public/js/driver.js
@@ -0,0 +1,12 @@
+frappe.ui.form.on("Driver", {
+    setup: function(frm) {
+        frm.set_query("transporter", function() {
+            return {
+                filters: {
+                    "is_transporter": 1,
+                    "disabled": 0
+                }
+            };
+        });
+    }
+});
---
+++ icd_tz/public/js/vehicle.js
@@ -0,0 +1,12 @@
+frappe.ui.form.on("Vehicle", {
+    setup: function(frm) {
+        frm.set_query("vehicle_owner", function() {
+            return {
+                filters: {
+                    "is_transporter": 1,
+                    "disabled": 0
+                }
+            };
+        });
+    }
+});
