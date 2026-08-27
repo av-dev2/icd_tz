0f358534962e98c77b5124f6d0bf2263f16dde82
av-dev2 <emgani@aakvatech.com>
2026-08-27 17:59:21 +0300

    test: cover Security officer naming

--- icd_tz/icd_tz/doctype/security_officer/test_security_officer.py
+++ icd_tz/icd_tz/doctype/security_officer/test_security_officer.py
@@ -1,9 +1,20 @@
 # Copyright (c) 2024, elius mgani and Contributors
 # See license.txt
 
-# import frappe
-from frappe.tests.utils import FrappeTestCase
+import frappe
+from frappe.tests import IntegrationTestCase
 
 
-class TestSecurityofficer(FrappeTestCase):
-	pass
+class TestSecurityofficer(IntegrationTestCase):
+	def tearDown(self):
+		frappe.db.rollback()
+
+	def test_an_officer_is_named_after_itself(self):
+		officer = frappe.get_doc({"doctype": "Security officer", "full_name": "ICD Test Officer"}).insert(
+			ignore_permissions=True
+		)
+
+		self.assertEqual(officer.name, "ICD Test Officer")
+
+	def test_an_officer_without_a_name_is_rejected(self):
+		self.assertRaises(frappe.ValidationError, frappe.get_doc({"doctype": "Security officer"}).insert)
