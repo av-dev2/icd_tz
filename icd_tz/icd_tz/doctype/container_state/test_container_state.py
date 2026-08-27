8d7ee8c1301fc6561da806835b6e048e1d6ac3ff
av-dev2 <emgani@aakvatech.com>
2026-08-27 17:59:07 +0300

    test: cover Container State naming

--- icd_tz/icd_tz/doctype/container_state/test_container_state.py
+++ icd_tz/icd_tz/doctype/container_state/test_container_state.py
@@ -1,9 +1,20 @@
 # Copyright (c) 2024, elius mgani and Contributors
 # See license.txt
 
-# import frappe
-from frappe.tests.utils import FrappeTestCase
+import frappe
+from frappe.tests import IntegrationTestCase
 
 
-class TestContainerState(FrappeTestCase):
-	pass
+class TestContainerState(IntegrationTestCase):
+	def tearDown(self):
+		frappe.db.rollback()
+
+	def test_a_state_is_named_after_itself(self):
+		state = frappe.get_doc({"doctype": "Container State", "state": "ICD Test Dented"}).insert(
+			ignore_permissions=True
+		)
+
+		self.assertEqual(state.name, "ICD Test Dented")
+
+	def test_a_state_without_a_name_is_rejected(self):
+		self.assertRaises(frappe.ValidationError, frappe.get_doc({"doctype": "Container State"}).insert)
