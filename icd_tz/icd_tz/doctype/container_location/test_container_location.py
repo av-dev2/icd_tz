1f5c5e31581d43254398411706b97d1e0525159a
av-dev2 <emgani@aakvatech.com>
2026-08-27 18:06:44 +0300

    test: cover Container Location naming

--- icd_tz/icd_tz/doctype/container_location/test_container_location.py
+++ icd_tz/icd_tz/doctype/container_location/test_container_location.py
@@ -1,9 +1,21 @@
 # Copyright (c) 2024, elius mgani and Contributors
 # See license.txt
 
-# import frappe
-from frappe.tests.utils import FrappeTestCase
+import frappe
+from frappe.tests import IntegrationTestCase
 
 
-class TestContainerLocation(FrappeTestCase):
-	pass
+class TestContainerLocation(IntegrationTestCase):
+	def tearDown(self):
+		frappe.db.rollback()
+
+	def test_a_location_is_named_after_itself(self):
+		location = frappe.get_doc(
+			{"doctype": "Container Location", "location_name": "ICD Test Bay 9"}
+		).insert(ignore_permissions=True)
+
+		self.assertEqual(location.name, "ICD Test Bay 9")
+
+	def test_a_location_without_a_name_is_rejected(self):
+		location = frappe.get_doc({"doctype": "Container Location"})
+		self.assertRaises(frappe.ValidationError, location.insert)
