8004c79835fe703c7fc132555abef9c22bddd8d9
av-dev2 <emgani@aakvatech.com>
2026-09-25 14:38:32 +0300

    fix: drop the custom verification lookup from the inspection form

--- icd_tz/icd_tz/doctype/container_inspection/container_inspection.js
+++ icd_tz/icd_tz/doctype/container_inspection/container_inspection.js
@@ -35,29 +35,6 @@ frappe.ui.form.on("Container Inspection", {
       };
     });
   },
-  in_yard_container_booking: (frm) => {
-    frm.trigger("get_container_custom_verification");
-  },
-  get_container_custom_verification: (frm) => {
-    if (frm.doc.in_yard_container_booking) {
-      frappe.call({
-        method: "get_custom_verification_services",
-        doc: frm.doc,
-        args: {
-          // self: frm.doc,
-          caller: "Front End",
-        },
-        callback: (r) => {
-          if (r.message) {
-            frm.add_child("services", {
-              service: r.message,
-            });
-            frm.refresh_field("services");
-          }
-        },
-      });
-    }
-  },
   create_additional_booking: (frm) => {
     if (frm.doc.docstatus != 1) {
       return;
